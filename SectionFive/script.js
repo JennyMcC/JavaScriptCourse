ea23ace332e3d07e9b9b46b92bfa4f9086ca439e
JennyMcC <jennymccargish@gmail.com>
2017-08-19 16:11:43 -0400

    another lesson

--- SectionFive/script.js
+++ SectionFive/script.js
@@ -36,6 +36,7 @@ console.log(katie.lastName);
 
 //////////////////////////////////////////////
 // Object.create
+/*
 var personProto = {
 	calculateAge: function() {
 		console.log(2017 - this.yearOfBirth)
@@ -52,11 +53,47 @@ var jimmy = Object.create(personProto, {
 	yearOfBirth: { value: 1986 },
 	job: { value: 'health educator'}
 });
+*/
 
 
 
 
+/////////////////////////////////////////
+// Primatives vs Objects
 
+// Primatives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);  // prints 46
+console.log(b);  // prints 23
+
+// Objects
+var obj1 = {
+	name: 'Jenny',
+	age: 33
+};
+var obj2 = obj1;
+obj1.age = 29;
+console.log(obj1.age); //both will print 29. WAT.
+console.log(obj2.age);
+
+// Functions
+var age = 21;
+var obj = {
+	name: 'Katie',
+	city: 'Oxford'
+};
+
+function change(a, b) {
+	a = 30;
+	b.city = 'Eaton';
+}
+
+change(age, obj);
+
+console.log(age);  // prints 30 be age is a primative and can't be changed
+console.log(obj.city);  // prints Eaton bc it's inside the object
 
 
 

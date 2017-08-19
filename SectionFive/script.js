1b6798d0f062ea431c5f8982d0f25493fa421a27
JennyMcC <jennymccargish@gmail.com>
2017-08-19 12:39:26 -0400

    OOP

---
+++ SectionFive/script.js
@@ -0,0 +1,50 @@
+// Function constructor
+
+var jenny = {
+	name: 'Jenny',
+	yearOfBirth: 1984,
+	job: 'developer'
+};
+// Function constructor names get capitalized
+var Person = function(name, yearOfBirth, job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
+}
+// OR I could have put this function in the constructor above with 'this.' instead of 'Person.prototype'
+Person.prototype.calculateAge = function() {
+		console.log(2017 - this.yearOfBirth)
+	};
+// can add a common property this way with 'prototype'
+Person.prototype.lastName = 'McCargish';
+
+var jenny = new Person('Jenny', 1984, 'developer');
+var jimmy = new Person('Jimmy', 1986, 'health educator');
+var katie = new Person('Katie', 1996, 'student');
+
+jenny.calculateAge();
+jimmy.calculateAge();
+katie.calculateAge();
+
+console.log(jenny.lastName);
+console.log(jimmy.lastName);
+console.log(katie.lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

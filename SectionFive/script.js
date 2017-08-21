c1a8a450f72751f2176146afe05dc75015d46510
JennyMcC <jennymccargish@gmail.com>
2017-08-21 18:26:16 -0400

    closures

--- SectionFive/script.js
+++ SectionFive/script.js
@@ -189,7 +189,7 @@ function game() {
 }
 game();
 */
-
+/*
 //must be in () to be treated as an expression
 (function () {
 	var score = Math.random() * 10;
@@ -204,9 +204,66 @@ game();
 	console.log(score >= 5 - goodLuck);
 })(10); //10 = goodLuck
 //can only use this one time.
+*/
 
 
 
+
+///////////////////////////////////////////////////
+// Lecture: Closures
+
+function retirement(retirementAge) {
+	return function(yearOfBirth) {
+		var a = ' years left until retirement.';
+		var age = 2017 - yearOfBirth;
+		console.log((retirementAge - age) + a);
+	}	
+}
+
+retirement(65)(1984); //age is 65 in Europe
+retirement(66)(1996);
+
+// setting the retirement age to be the same using a new name:
+var retirementUS = retirement(66); //66 in USA
+// now I don't have to enter the retirement age:
+retirementUS(1986);
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1984);
+retirementIceland(1984);
+
+
+
+// Challenge: rewrite interview problem.
+// What I did:
+function interview(job) {
+	return function(name) {
+		console.log(name + job);
+	}
+}
+
+var interviewDesigner = interview(', can you please explain what UX design is?')
+var interviewTeacher = interview(', what subject do you teach?');
+
+interviewTeacher('Ashley');
+interviewDesigner('Jenny');
+
+// What he did:
+function interviewQuestion(job) {
+	return function(name) {
+		if (job === 'designer') {
+			console.log(name + ', can you please explain what UX design is?');
+		} else if (job === 'teacher') {
+			console.log('What subject do you teach, ' + name + '?');
+		} else {
+			console.log('Hello ' + name + '. What do you do?');
+		}
+	}	
+}
+
+interviewQuestion('teacher')('Molly');
 
 
 

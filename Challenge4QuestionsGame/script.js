e61ef22a53e50374e6932ad6e4e264f127185477
JennyMcC <jennymccargish@gmail.com>
2017-08-22 16:26:55 -0400

    almost got it

--- Challenge4QuestionsGame/script.js
+++ Challenge4QuestionsGame/script.js
@@ -6,22 +6,36 @@ var Question = function(questionAsked, options, correctAnswer) {
 	this.correctAnswer = correctAnswer;
 }
 
-var questionOne = new Question('Who is the creator of this game?', '1.Your Mom 2.Jenny 3.Stephen Hawking, obviously', 2);
-var questionTwo = new Question('How many siblings does Jenny have?', '1.Two 2.Zero 3.Four', 1);
-var questionThree = new Question('Where did Jenny go to college?', '1.University of Miami 2.Harvard 3.Miami University', 3);
+var questionOne = new Question('Who is the creator of this game?', '1.Your Mom  2.Jenny  3.Stephen Hawking, obviously', 2);
+var questionTwo = new Question('How many siblings does Jenny have?', '1.Two  2.Zero  3.Four', 1);
+var questionThree = new Question('Where did Jenny go to college?', '1.University of Miami  2.Harvard  3.Miami University', 3);
+
+/*
+var myQuestions = [];
+myQuestions.push(questionOne);
+myQuestions.push(questionTwo);
+myQuestions.push(questionThree);
+*/
 
 var whichQuestion = Math.floor(Math.random() * 3) + 1;
 		if (whichQuestion === 1) {
-			console.log(questionOne.questionAsked + ' ' + questionOne.options);
+			console.log(questionOne.questionAsked);
+			console.log(questionOne.options);
 		} else if (whichQuestion === 2) {
-			console.log(questionTwo.questionAsked + ' ' + questionTwo.options);
+			console.log(questionTwo.questionAsked);
+			console.log(questionTwo.options);
 		} else if (whichQuestion === 3) {
-			console.log(questionThree.questionAsked + ' ' + questionThree.options);
+			console.log(questionThree.questionAsked);
+			console.log(questionThree.options);
 		}
 	
-
-
-
-
+var enterAnswer = prompt('Enter the number of the correct answer:');
+	//for (var i = 0; i <= myQuestions.length; i++) {
+		if (enterAnswer === this.correctAnswer) {
+			console.log('Correct!');
+		} else if (enterAnswer !== this.correctAnswer) {
+			console.log('You know nothing.');
+		}
+	//}
 
 

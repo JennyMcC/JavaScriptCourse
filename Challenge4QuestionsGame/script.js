a664ef658d04e4be164a97c64fad464401e37fbc
JennyMcC <jennymccargish@gmail.com>
2017-08-22 15:14:16 -0400

    new challenge

---
+++ Challenge4QuestionsGame/script.js
@@ -0,0 +1,30 @@
+// Challenge 4: Questions Game
+
+var Question = function(questionAsked, options[], correctAnswer) {
+	this.questionAsked = questionAsked;
+	this.options[] = options[];
+	this.correctAnswer = correctAnswer;
+}
+
+var questionOne = new Question('Who is the creator of this game?', ['Your Mom', 'Jenny', 'Stephen Hawking, obviously'], 1);
+var questionTwo = new Question('How many siblings does Jenny have?', [2, 0, 4], 0);
+var questionThree = new Question('Where did Jenny go to college?', ['University of Miami', 'Harvard', 'Miami University'], 2);
+
+
+
+
+
+
+
+/*
+whichQuestion: function(Math.random * 3) {
+		if (Math.random === 1) {
+			console.log(options[0])
+		}
+	} */
+
+
+
+
+
+

2fe4584d0bc61702934c2fb2e8443a08e8a650a3
JennyMcC <jennymccargish@gmail.com>
2017-08-22 15:33:20 -0400

    asking questions

--- Challenge4QuestionsGame/script.js
+++ Challenge4QuestionsGame/script.js
@@ -1,27 +1,24 @@
 // Challenge 4: Questions Game
 
-var Question = function(questionAsked, options[], correctAnswer) {
+var Question = function(questionAsked, options, correctAnswer) {
 	this.questionAsked = questionAsked;
-	this.options[] = options[];
+	this.options = options;
 	this.correctAnswer = correctAnswer;
 }
 
-var questionOne = new Question('Who is the creator of this game?', ['Your Mom', 'Jenny', 'Stephen Hawking, obviously'], 1);
-var questionTwo = new Question('How many siblings does Jenny have?', [2, 0, 4], 0);
-var questionThree = new Question('Where did Jenny go to college?', ['University of Miami', 'Harvard', 'Miami University'], 2);
+var questionOne = new Question('Who is the creator of this game?', '1.Your Mom 2.Jenny 3.Stephen Hawking, obviously', 2);
+var questionTwo = new Question('How many siblings does Jenny have?', '1.Two 2.Zero 3.Four', 1);
+var questionThree = new Question('Where did Jenny go to college?', '1.University of Miami 2.Harvard 3.Miami University', 3);
 
-
-
-
-
-
-
-/*
-whichQuestion: function(Math.random * 3) {
-		if (Math.random === 1) {
-			console.log(options[0])
+var whichQuestion = Math.floor(Math.random() * 3) + 1;
+		if (whichQuestion === 1) {
+			console.log(questionOne.questionAsked + ' ' + questionOne.options);
+		} else if (whichQuestion === 2) {
+			console.log(questionTwo.questionAsked + ' ' + questionTwo.options);
+		} else if (whichQuestion === 3) {
+			console.log(questionThree.questionAsked + ' ' + questionThree.options);
 		}
-	} */
+	
 
 
 

513c391272355e59d84f5581a463bd3eec1bdf02
JennyMcC <jennymccargish@gmail.com>
2017-08-23 10:33:19 -0400

    expert answer

--- Challenge4QuestionsGame/script.js
+++ Challenge4QuestionsGame/script.js
@@ -33,7 +33,7 @@ var enterAnswer = prompt('Enter the number of the correct answer:');
 		} else if (enterAnswer !== this.correctAnswer) {
 			console.log('You know nothing.');
 		}
-	//}
+	//} Always says you are wrong..
 */
 
 
@@ -41,8 +41,60 @@ var enterAnswer = prompt('Enter the number of the correct answer:');
 
 /////////////////////////////////////////////////////////////////
 // His solution:
-
+/*
 // ensures code is private and doesn't interfere with other programmers code (IIFE):
+(function() {
+function Question(question, answers, correct) {
+	this.question = question;
+	this.answers = answers;
+	this.correct = correct;
+}
+
+Question.prototype.displayQuestion = function() {
+	console.log(this.question);
+
+	for (var i = 0; i < this.answers.length; i++) {
+		console.log(i + ': ' + this.answers[i]);
+	}
+}
+
+Question.prototype.checkAnswer = function(ans) {
+	if (ans === this.correct) {
+		console.log('Correct Answer!');
+	} else {
+		console.log('Wrong answer. Try again.');
+	}
+}
+
+var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+						['Yes', 'No'],
+						0);
+var q2 = new Question('What is the name of this course\'s teacher?', 
+						['John', 'Michael', 'Jonas'],
+						2);
+var q3 = new Question('What word best describes coding?',
+						['Boring', 'Hard', 'Fun', 'Tedious'],
+						2);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer:'));
+
+questions[n].checkAnswer(answer);
+})();
+*/
+
+
+
+
+
+///////////////////////////////////////////////
+// 'Expert Level' Solution:
+
 (function() {
 function Question(question, answers, correct) {
 	this.question = question;

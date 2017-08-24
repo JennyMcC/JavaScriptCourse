797ef28741d661b9cb526973c66868a3a92396e0
JennyMcC <jennymccargish@gmail.com>
2017-08-24 19:58:09 -0400

    first steps

--- SectionSix/app.js
+++ SectionSix/app.js
@@ -1,2 +1,42 @@
-///////////////////////////////////////////
-// Budgety page:
+//IIFE
+var budgetController = (function() {
+//the following is private bc it is in the closure:
+//can only call on it publicly by budgetController.publicTest(b) (this will essentially add b to 23)
+	var x = 23;
+
+	var add = function(a) {
+		return x + a;
+	}
+
+	return {
+		publicTest: function(b) {
+			return add(b);
+		}
+	}
+
+})();
+
+
+
+
+//another IIFE (to separate this code from the last code)
+var UIController = (function() {
+	//some code
+
+})();
+
+
+
+
+//another IIFE
+var controller = (function(budgetCtrl, UICtrl) {
+
+	var z = budgetCtrl.publicTest(5);
+	return {
+		anotherPublic: function() {
+			console.log(z);
+		}
+	}
+
+})(budgetController, UIController);
+

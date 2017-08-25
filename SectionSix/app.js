6173b357e5f7ba114e71920dc365619880d18fc3
JennyMcC <jennymccargish@gmail.com>
2017-08-25 11:20:42 -0400

    calling on user input

--- SectionSix/app.js
+++ SectionSix/app.js
@@ -12,8 +12,31 @@ var budgetController = (function() {
 // UI CONTROLLER
 //another IIFE (to separate this code from the last code)
 var UIController = (function() {
-	
-	//some code
+	// storing class names from HTML so if one changes, I don't have to change it throughout the JS code:
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	};
+
+	return {
+		
+		getInput: function() {
+			// getting the 3 things the user inputs at the top of the page:
+			return {
+
+				type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+				description: document.querySelector(DOMstrings.inputDescription).value, // value is simply what is entered by user
+				value: document.querySelector(DOMstrings.inputValue).value
+			
+			};
+		},
+		// exposing DOMstrings to the public so other contollers can use them:
+		getDOMstrings: function() {
+			return DOMstrings;
+		}
+	};
 
 })();
 
@@ -23,9 +46,13 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 //another IIFE; calling on the previous ones to connect them:
 var controller = (function(budgetCtrl, UICtrl) {
+	// calling on the DOMstrings so I can use them here:
+	var DOM = UICtrl.getDOMstrings();
 
 	var ctrlAddItem = function() {
 		// 1. Get the field input data
+		var input = UICtrl.getInput();
+		console.log(input);
 
 		// 2. Add the item to the budget controller
 
@@ -34,10 +61,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 4. Calculate the budget
 
 		// 5. Display the budget on the UI
-		
-	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	}
+	// now called DOM instead of DOMstrings bc I changed it when I called on it. whyyyyy
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	// Adding another eventListener for hitting the return key (keypress event) instead of clicking the button:
 	document.addEventListener('keypress', function(event) {
@@ -47,6 +74,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 			ctrlAddItem();
 
 		}
+
 	});
 
 })(budgetController, UIController);

1e6aa3f0a751cc326ac3d265d8efa4adb6050839
JennyMcC <jennymccargish@gmail.com>
2017-08-25 10:48:18 -0400

    building onto budget app

--- SectionSix/app.js
+++ SectionSix/app.js
@@ -1,26 +1,18 @@
-//IIFE
+//IIFE BUDGET CONTROLLER
 var budgetController = (function() {
 //the following is private bc it is in the closure:
 //can only call on it publicly by budgetController.publicTest(b) (this will essentially add b to 23)
-	var x = 23;
-
-	var add = function(a) {
-		return x + a;
-	}
-
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	}
+	
 
 })();
 
 
 
 
+// UI CONTROLLER
 //another IIFE (to separate this code from the last code)
 var UIController = (function() {
+	
 	//some code
 
 })();
@@ -28,15 +20,46 @@ var UIController = (function() {
 
 
 
-//another IIFE
+// GLOBAL APP CONTROLLER
+//another IIFE; calling on the previous ones to connect them:
 var controller = (function(budgetCtrl, UICtrl) {
 
-	var z = budgetCtrl.publicTest(5);
-	return {
-		anotherPublic: function() {
-			console.log(z);
-		}
+	var ctrlAddItem = function() {
+		// 1. Get the field input data
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
+		
 	}
 
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	// Adding another eventListener for hitting the return key (keypress event) instead of clicking the button:
+	document.addEventListener('keypress', function(event) {
+		// specifying return key (enter's keycode in the console..older browsers use 'which')
+		if (event.keycode === 13 || event.which === 13) {
+			
+			ctrlAddItem();
+
+		}
+	});
+
 })(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
 

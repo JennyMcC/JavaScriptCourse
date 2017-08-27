602e6291ff86a6dfaa8f135a0d487983ea561b8e
JennyMcC <jennymccargish@gmail.com>
2017-08-27 13:01:42 -0400

    blocking empty fields

--- SectionSix/app.js
+++ SectionSix/app.js
@@ -77,7 +77,8 @@ var UIController = (function() {
 			return {
 				type: document.querySelector(DOMstrings.inputType).value, // will be either inc. or exp.
 				description: document.querySelector(DOMstrings.inputDescription).value, // value is simply what is entered by user
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+				// parseFloat will convert this from a string to a number so we can add with it later.
 			};
 		},
 
@@ -148,23 +149,35 @@ var controller = (function(budgetCtrl, UICtrl) {
 	};
 
 
+	var updateBudget = function() {
+
+		// 1. Calculate the budget
+
+		// 2. return the budget
+
+		// 3. Display the budget on the UI
+
+	};
+
+
 	var ctrlAddItem = function() {
 		var input, newItem;
 		// 1. Get the field input data
 		input = UICtrl.getInput();
+		// only adding things if the descriptions are actually filled out AND if the value is NOT Not a Number AND if the value is greater than 0:
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+			// 2. Add the item to the budget controller
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-		// 2. Add the item to the budget controller
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+			// 3. Add the item to the UI
+			UICtrl.addListItem(newItem, input.type);
 
-		// 3. Add the item to the UI
-		UICtrl.addListItem(newItem, input.type);
+			// 4. Clear the fields:
+			UICtrl.clearFields();
 
-		// 4. Clear the fields:
-		UICtrl.clearFields();
-
-		// 5. Calculate the budget
-
-		// 6. Display the budget on the UI
+			// 5. Calculate and update budget:
+			updateBudget();
+		}
 
 	};
 

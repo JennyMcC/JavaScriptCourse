e0922f7042b5c60f1484f3e5e77bf808b93b287b
JennyMcC <jennymccargish@gmail.com>
2017-08-27 12:39:47 -0400

    clearing and resetting fields

--- SectionSix/app.js
+++ SectionSix/app.js
@@ -104,6 +104,21 @@ var UIController = (function() {
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 		},
 
+		clearFields: function() {
+			var fields, fieldsArr;
+
+			fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+			// changing the object fields to an array (using slice):
+			fieldsArr = Array.prototype.slice.call(fields);
+			// resetting the values so they're empty:
+			fieldsArr.forEach(function(current, index, array) {
+				current.value = "";
+			});
+			// setting the curser to be back on the first field (using focus..focus on the first field in the array):
+			fieldsArr[0].focus();
+
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -144,9 +159,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 3. Add the item to the UI
 		UICtrl.addListItem(newItem, input.type);
 
-		// 4. Calculate the budget
+		// 4. Clear the fields:
+		UICtrl.clearFields();
 
-		// 5. Display the budget on the UI
+		// 5. Calculate the budget
+
+		// 6. Display the budget on the UI
 
 	};
 

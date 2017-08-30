32da3994f933153ecff2198b115991dbfe61d314
JennyMcC <jennymccargish@gmail.com>
2017-08-30 19:54:32 -0400

    adding the date

--- SectionSix/app.js
+++ SectionSix/app.js
@@ -149,7 +149,8 @@ var UIController = (function() {
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	};
 
 	// need to call on the number and the type bc it will either be a '+' or a '-':
@@ -250,6 +251,16 @@ var UIController = (function() {
 				}
 			});
 		},
+		//displaying the month at the top:
+		displayMonth: function() {
+			var now, months, month, year;
+			now = new Date(); //this will simply return todays date
+			//returning the names of the months so it won't print them by #
+			months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			month = now.getMonth(); //returns current month
+			year = now.getFullYear(); //returns current year (in full)
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
 
 		getDOMstrings: function() {
 			return DOMstrings;
@@ -341,6 +352,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 	return {
 		init: function() {
 			console.log('application has started.');
+			UICtrl.displayMonth();
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,

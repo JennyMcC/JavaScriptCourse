670099b97e7e0c56ddd1361cb7c0de402d6c7fc2
JennyMcC <jennymccargish@gmail.com>
2017-09-06 11:49:59 -0400

    ES6 maps

--- Section7/script.js
+++ Section7/script.js
@@ -483,7 +483,7 @@ var amy = new McCargishPerson('Amy', 1970, 'Bowser');
 
 
 // ES6
-
+/*
 function McCargishPerson(firstName, yearOfBirth, lastName = 'McCargish', nationality = 'American') {
 
 	this.firstName = firstName;
@@ -495,6 +495,55 @@ function McCargishPerson(firstName, yearOfBirth, lastName = 'McCargish', nationa
 var jenny = new McCargishPerson('Jenny', 1984);
 var katie = new McCargishPerson('Katie', 1996);
 var amy = new McCargishPerson('Amy', 1970, 'Bowser');
+*/
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// Lecture: Maps
+
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer!');
+question.set(false, 'Wrong, please try again.');
+
+console.log(question.get('question'));
+console.log(question.get(1));
+//console.log(question.size); //how many? (8)
+
+if (question.has(4)) {
+	//question.delete(4);
+	//console.log('Answer 4 is here')
+}
+//OR
+//question.delete(4);
+//OR, to clear ALL:
+//question.clear();
+
+//prints all elements(loops through):
+//question.forEach((value, key) => console.log(`This is ${key} and it's set to ${value}`));
+
+
+for (let [key, value] of question.entries()) {
+	if (typeof(key) === 'number') {
+		console.log(`Answer ${key}: ${value}`);
+	}
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+
+console.log(question.get(ans === question.get('correct')));
+
+
+
 
 
 
